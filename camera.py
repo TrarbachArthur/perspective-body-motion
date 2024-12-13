825d8759482a4d8b87bafd3fc033c894fc24133d
TrarbachArthur <arthurtrarbach.sampaio@gmail.com>
2024-12-13 15:26:57 -0300

    :sparkles: Camera and some transformations

---
+++ camera.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+class Camera:
+    def __init__(self):
+        # initialize camera at origin
+        self.M = np.eye(4)
+        
+        self.image = (1280,720)
+        self.ccd = (36,24)
+        self.focal_dist = 50
+        self.stheta = 0
+
+    def get_intrinsic_matrix(self):
+        # calculate the intrinsic matrix
+        f = self.focal_dist
+        stheta = self.stheta
+        sx = self.image[0]/self.ccd[0]
+        sy = self.image[1]/self.ccd[1]
+        cx = self.image[0]/2
+        cy = self.image[1]/2
+
+        return np.array([[f*sx, f*stheta, f*cx],
+                         [0, f*sy, f*cy],
+                         [0, 0, 1]])
+    
+    def update_intrinsic(self, image=None, ccd=None, fd=None, stheta=None):
+        self.image = image if image != None else self.image
+        self.ccd = ccd if ccd != None else self.ccd 
+        self.focal_dist = fd if fd != None else self.focal_dist
+        self.stheta = stheta if stheta != None else self.stheta
+
+        print(self.image, self.ccd, self.focal_dist, self.stheta)
